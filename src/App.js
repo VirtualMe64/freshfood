b0ebde3898eb8ada26445f06134e6088ccd69980
VirtualMe64 <43835028+VirtualMe64@users.noreply.github.com>
2022-08-05 09:49:06 -0400

    Everything

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Polyline,
+} from "react-leaflet";
 
-function App() {
+var data = require("./data/Farmers_Markets.json");
+var wardData = require("./data/Wards.json");
+
+const App = () => {
+  const markets = data["features"];
+  const wards = wardData["features"];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <MapContainer center={[38.9072, -77.0369]} zoom={13} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {markets.map((market) => (
+        <Marker
+          position={
+            [
+              ...market["geometry"]["coordinates"],
+            ].reverse() /* deep copy and reverse */
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Popup>
+            <a
+              href={market["properties"]["WEB_URL"]}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {market["properties"]["NAME"]}
+            </a>
+          </Popup>
+        </Marker>
+      ))}
+      {wards.map((w) => (
+        <Polyline
+          positions={w["geometry"]["coordinates"][0].map((c) =>
+            [...c].reverse()
+          )}
+          pathOptions={{ color: "black", weight: 1 }}
+          smoothFactor={1}
+        />
+      ))}
+    </MapContainer>
   );
-}
+};
 
 export default App;
